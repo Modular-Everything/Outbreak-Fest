920ad6a98c26ead4a50a5b46eccee06c1d094dae
Chrish Dunne <hey@chrish.design>
2020-07-20 09:52:11 +0100

    [+] Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Outbreak Fest 2021`,
+    description: `Hardcore music festival.`,
+    author: `@chrishd`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: 'AuU2GoTntmHoz7Ovj9wh9gtt',
+        resolveLinks: true,
+        // resolveRelations: [],
+        homeSlug: 'home',
+        version: process.env.NODE_ENV === `production` ? `published` : `draft`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +22,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1D4EA7`,
+        theme_color: `#1D4EA7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/outbreak-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
   ],
-}
+};
