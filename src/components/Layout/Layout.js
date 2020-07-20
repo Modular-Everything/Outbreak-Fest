deac331d0b94943fb5d845e801fe3707c423daf3
Chrish Dunne <hey@chrish.design>
2020-07-20 19:41:03 +0100

    [+] Added rips

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,10 @@ import { useStaticQuery, graphql } from 'gatsby';
 import '../../styles/styles.css';
 import Header from '../Header/Header';
 import Texture from '../Texture/Texture';
+import Hero from '../Hero/Hero';
 import Footer from '../Footer/Footer';
+
+// ====
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,6 +23,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Hero />
       <Header siteTitle={data.site.siteMetadata.title} />
       <main>
         <div className="container">{children}</div>
@@ -30,8 +34,10 @@ const Layout = ({ children }) => {
   );
 };
 
+export default Layout;
+
+// ====
+
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export default Layout;
---
+++ src/components/Rips/Rips.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import RipImg from '../../images/rips.png';
+
+// ====
+
+const Rips = ({ children }) => {
+  return (
+    <>
+      <Rip BgSrc={RipImg}>Rip</Rip>
+      {children && <Content>{children}</Content>}
+      <Rip BgSrc={RipImg} flip>
+        Rip
+      </Rip>
+    </>
+  );
+};
+
+export default Rips;
+
+// ====
+
+const Rip = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-image: url(${({ BgSrc }) => BgSrc});
+  background-position: bottom;
+  background-repeat: repeat-x;
+  background-size: 40%;
+  transform: ${({ flip }) => (flip ? `rotate(180deg)` : `none`)};
+`;
+
+const Content = styled.section`
+  background-color: #fff;
+`;
+
+// ====
+
+Rips.propTypes = {
+  children: PropTypes.node,
+};
+
+Rips.defaultProps = {
+  children: null,
+};
