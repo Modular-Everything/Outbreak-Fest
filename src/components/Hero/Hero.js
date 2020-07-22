ec05dbd3f6031ef3019666462c3a64e3595ee16b
Chrish Dunne <hey@chrish.design>
2020-07-22 21:39:10 +0100

    [+] Added hero images

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
 
-import ImgA from '../../images/brand-image_1.png';
-import ImgB from '../../images/brand-image_2.png';
-import ImgC from '../../images/brand-image_3.png';
 import Rips from '../Rips/Rips';
 
 // ====
 
 const Hero = () => {
+  const DATA = useStaticQuery(graphql`
+    query HeroImagesQ {
+      allFile(limit: 3, filter: { name: { glob: "brand-image_*" } }) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <section>
-      <Rips />
-    </section>
+    <HeroWrap>
+      <HeroImages>
+        {DATA.allFile.edges.map((node) => {
+          return (
+            <li>
+              <Img fluid={node.node.childImageSharp.fluid} alt="" />
+            </li>
+          );
+        })}
+      </HeroImages>
+
+      <RipDivide>
+        <Rips />
+      </RipDivide>
+    </HeroWrap>
   );
 };
 
 export default Hero;
+
+// ====
+
+const HeroWrap = styled.section`
+  overflow: hidden;
+`;
+
+const HeroImages = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1.5rem;
+  width: calc(100vw - 1.5rem * 2);
+  padding: 0;
+  margin: 0 1.5rem;
+  list-style: none;
+
+  & li img {
+    width: 100%;
+    mix-blend-mode: screen;
+  }
+`;
+
+const RipDivide = styled.div`
+  position: relative;
+  top: -4rem;
+`;
