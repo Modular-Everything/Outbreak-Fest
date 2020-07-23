6f3d7a1f7e3836ae8219052324b1c45e69009c29
Chrish Dunne <hey@chrish.design>
2020-07-23 15:29:00 +0100

    [-] Remove unused component

--- src/pages/info.js
+++ src/pages/info.js
@@ -4,7 +4,6 @@ import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO/SEO';
 import BigDate from '../components/BigDate/BigDate';
 import Divider from '../components/Divider/Divider';
-import Poster from '../components/Poster/Poster';
 import SocialMedia from '../components/SocialMedia/SocialMedia';
 
 // ====
