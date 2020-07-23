279e0aa8bbaf6408fb2c76ef619450ad3eaf24e2
Chrish Dunne <hey@chrish.design>
2020-07-23 11:02:27 +0100

    [+] Added basic footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // ====
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">© {new Date().getFullYear()}</div>
-    </footer>
+    <FooterWrap>
+      <div className="container">
+        <P>© {new Date().getFullYear()}</P>
+      </div>
+    </FooterWrap>
   );
 };
 
 export default Footer;
+
+// ====
+
+const FooterWrap = styled.footer`
+  margin-bottom: 4.5rem;
+`;
+
+const P = styled.p`
+  font-size: 1.125;
+  text-align: center;
+  margin: 0;
+  color: var(--light);
+`;
