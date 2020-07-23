ccdf5b8d5b97e1466d7a4d501798f52d7b765d56
Chrish Dunne <hey@chrish.design>
2020-07-23 15:44:47 +0100

    [+] Added missing page

---
+++ src/pages/tickets.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Layout from '../components/Layout/Layout';
+import SEO from '../components/SEO/SEO';
+import BigDate from '../components/BigDate/BigDate';
+import Divider from '../components/Divider/Divider';
+import SocialMedia from '../components/SocialMedia/SocialMedia';
+
+// ====
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Divider />
+    <BigDate />
+    <Divider />
+    Tickets
+    <SocialMedia />
+  </Layout>
+);
+
+export default IndexPage;
