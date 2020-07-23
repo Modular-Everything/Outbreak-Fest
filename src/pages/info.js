b03546dc14267b829cdb2acbd90052a39e58b993
Chrish Dunne <hey@chrish.design>
2020-07-23 15:28:43 +0100

    [+] Added missing pages

---
+++ src/pages/info.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Layout from '../components/Layout/Layout';
+import SEO from '../components/SEO/SEO';
+import BigDate from '../components/BigDate/BigDate';
+import Divider from '../components/Divider/Divider';
+import Poster from '../components/Poster/Poster';
+import SocialMedia from '../components/SocialMedia/SocialMedia';
+
+// ====
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Divider />
+    <BigDate />
+    <Divider />
+    Info
+    <SocialMedia />
+  </Layout>
+);
+
+export default IndexPage;
