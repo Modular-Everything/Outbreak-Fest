6fe8508509977ba8927e30b4c724ec7f7a7ea143
Chrish Dunne <hey@chrish.design>
2020-07-23 15:08:50 +0100

    [+] Added social icons

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+} from '@fortawesome/free-brands-svg-icons';
 
 import Rips from '../Rips/Rips';
 
@@ -27,7 +32,7 @@ const SocialMedia = () => {
           <Title>Latest from social media</Title>
           <Posts>
             {!insta.length ? (
-              <p>Loading...</p>
+              <Loading>Loading...</Loading>
             ) : (
               insta.map((gram) => (
                 <li key={gram.id}>
@@ -43,6 +48,26 @@ const SocialMedia = () => {
               ))
             )}
           </Posts>
+          <SocialIcons>
+            <li>
+              <a
+                href="https://facebook.com/outbreak.fest"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon icon={faFacebookSquare} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://instagram.com/outbreakfest"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon icon={faInstagramSquare} />
+              </a>
+            </li>
+          </SocialIcons>
         </div>
       </Rips>
     </div>
@@ -81,4 +106,45 @@ const Posts = styled.ul`
   & img {
     width: 100%;
   }
+`;
+
+const SocialIcons = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+  margin-top: 4rem;
+  list-style: none;
+
+  & li {
+    font-size: 2rem;
+    margin-left: 1rem;
+
+    &:nth-of-type(1) {
+      margin-left: 0;
+
+      a {
+        color: var(--primary);
+      }
+    }
+
+    &:nth-of-type(2) a {
+      color: var(--secondary);
+    }
+  }
+`;
+
+const Loading = styled.li`
+  display: block;
+  align-self: center;
+  font-family: 'EurostileExt', 'Helvetica Neue', Helvetica, sans-serif;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+  margin-top: 2rem;
+  color: var(--secondary);
+  grid-column: 2/4;
+  grid-row: 1/3;
 `;
