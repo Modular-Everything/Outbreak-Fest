62d6c56b96a9efc82241965965cd627bf2042053
Chrish Dunne <hey@chrish.design>
2020-07-29 21:05:59 +0100

    [+] Added burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
@@ -7,41 +7,65 @@ import OutbreakLogo from '../../images/logo.png';
 
 // ====
 
-const Header = ({ siteTitle }) => (
-  <header style={{ marginBottom: `4.5rem` }}>
-    <div className="container">
-      <Navigation role="navigation">
-        <ul>
-          <li>
-            <Link activeClassName="active" to="/lineup">
-              Line-up
+const Header = ({ siteTitle }) => {
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <header style={{ marginBottom: `4.5rem` }}>
+      <div className="container">
+        <Navigation role="navigation" open={menu}>
+          <MobileNav>
+            <Link to="/">
+              <img src={OutbreakLogo} alt={siteTitle} />
             </Link>
-          </li>
-          <li>
-            <Link activeClassName="active" to="/history">
-              History
-            </Link>
-          </li>
-          <li>
-            <Link activeClassName="active" to="/">
-              <Logo src={OutbreakLogo} alt={siteTitle} />
-            </Link>
-          </li>
-          <li>
-            <Link activeClassName="active" to="/tickets">
-              Tickets
-            </Link>
-          </li>
-          <li>
-            <Link activeClassName="active" to="/info">
-              Info
-            </Link>
-          </li>
-        </ul>
-      </Navigation>
-    </div>
-  </header>
-);
+            <button type="button" onClick={() => setMenu(true)}>
+              <svg viewBox="0 0 100 80" width="32" height="32" fill="#fff">
+                <rect width="100" height="10"></rect>
+                <rect y="30" width="100" height="10"></rect>
+                <rect y="60" width="100" height="10"></rect>
+              </svg>
+            </button>
+          </MobileNav>
+
+          <ul>
+            <li>
+              <Link activeClassName="active" to="/lineup">
+                Line-up
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName="active" to="/history">
+                History
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <Logo>
+                  <img src={OutbreakLogo} alt={siteTitle} />
+                </Logo>
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName="active" to="/tickets">
+                Tickets
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName="active" to="/info">
+                Info
+              </Link>
+            </li>
+            <li>
+              <button type="button" onClick={() => setMenu(false)}>
+                X
+              </button>
+            </li>
+          </ul>
+        </Navigation>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
 
@@ -49,19 +73,55 @@ export default Header;
 
 const Navigation = styled.nav`
   & ul {
-    display: grid;
-    grid-template-columns: 1fr 1fr 300px 1fr 1fr;
-    grid-gap: 3rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    display: ${({ open }) => (open ? `flex` : `none`)};
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
     margin: 0;
+    background-color: var(--primary);
     list-style: none;
+
+    @media (min-width: 1024px) {
+      position: relative;
+      display: grid;
+      grid-template-columns: 1fr 1fr 300px 1fr 1fr;
+      grid-gap: 3rem;
+      width: unset;
+      height: unset;
+    }
+
+    & li:nth-of-type(3) {
+      display: none;
+
+      @media (min-width: 1024px) {
+        display: unset;
+      }
+    }
   }
 
-  & a {
+  & li a,
+  & li button {
     font-family: 'EurostileExt', var(--font-family-sans-serif);
+    font-size: 10vw;
     font-weight: 900;
     text-transform: uppercase;
+    border: 0;
+    background: transparent;
     color: var(--light);
+    cursor: pointer;
     text-decoration: none;
+
+    @media (min-width: 1024px) {
+      font-size: 18px;
+      font-size: 1.125rem;
+    }
 
     &:hover {
       color: var(--secondary);
@@ -79,6 +139,11 @@ const Navigation = styled.nav`
   & li {
     display: flex;
     align-items: center;
+    margin-bottom: 1rem;
+
+    @media (min-width: 1024px) {
+      margin-bottom: 0;
+    }
   }
 
   & li:nth-of-type(1),
@@ -92,9 +157,33 @@ const Navigation = styled.nav`
   }
 `;
 
-const Logo = styled.img`
-  width: 100%;
-  mix-blend-mode: screen;
+const Logo = styled.div`
+  & img {
+    width: 100%;
+    mix-blend-mode: screen;
+  }
+`;
+
+const MobileNav = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
+
+  & button {
+    border: 0;
+    background: transparent;
+    cursor: pointer;
+  }
+
+  & img {
+    width: 100%;
+    max-width: 300px;
+    mix-blend-mode: screen;
+  }
 `;
 
 // ====
