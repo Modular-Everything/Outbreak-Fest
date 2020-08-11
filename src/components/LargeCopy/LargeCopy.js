106c4ff6a4bc5ee9ce88e18ee6665bb4d5231a6d
Chrish Dunne <hey@chrish.design>
2020-08-11 17:41:24 +0100

    [+] Added more info

--- src/components/LargeCopy/LargeCopy.js
+++ src/components/LargeCopy/LargeCopy.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 // ====
 
 const LargeCopy = ({ children }) => (
-  <div class="container">
+  <div className="container">
     <Copy>{children}</Copy>
   </div>
 );
@@ -14,6 +14,20 @@ export default LargeCopy;
 // ====
 
 const Copy = styled.section`
+  & h2 {
+    font-family: 'EurostileExt', Arial, Helvetica, sans-serif;
+    font-size: 2.5rem;
+    line-height: 1.2;
+    font-weight: bold;
+    text-transform: uppercase;
+    margin: 0;
+    color: var(--light);
+  }
+
+  & a {
+    color: var(--light);
+  }
+
   & ol {
     font-size: 1.125rem;
     color: var(--light);
