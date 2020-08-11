2a44ae5e578062560ac74e191016c86799d140a9
Chrish Dunne <hey@chrish.design>
2020-08-11 18:35:56 +0100

    [~] Splash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import Layout from '../components/Layout/Layout';
+import '../styles/styles.css';
+import OutbreakLogo from '../images/logo.png';
 import SEO from '../components/SEO/SEO';
-import BigCopy from '../components/BigCopy/BigCopy';
-import Divider from '../components/Divider/Divider';
-import Video from '../components/Video/Video';
-import SocialMedia from '../components/SocialMedia/SocialMedia';
 
 // ====
 
 const IndexPage = () => (
-  <Layout mask>
-    <SEO title="Home" />
-
-    <Divider />
-    <BigCopy />
-    <Divider />
-    <Video videoId="4auoaq1XmWQ" />
-    <SocialMedia />
-  </Layout>
+  <Wrapper>
+    <SEO title="Announcement Soon" />
+    <Logo src={OutbreakLogo} alt="Outbreak Fest 2021" />
+    <h2>Announcement soon</h2>
+  </Wrapper>
 );
 
 export default IndexPage;
+
+// ====
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+
+  & h2 {
+    font-family: 'EurostileExt', 'Helvetica Neue', sans-serif;
+    font-size: 1.13rem;
+    text-transform: uppercase;
+    margin-top: 0.3rem;
+    color: var(--secondary);
+  }
+`;
+
+const Logo = styled.img`
+  width: 100%;
+  max-width: 300px;
+  mix-blend-mode: screen;
+`;
