9016b8bbd976b7a4b0aa175d069f85fe6134b6ef
Chrish Dunne <hey@chrish.design>
2020-09-02 09:53:34 +0100

    [+] Added analytics

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 import OutbreakLogo from '../../images/logo.png';
 
@@ -9,9 +10,47 @@ import OutbreakLogo from '../../images/logo.png';
 
 const Header = ({ siteTitle }) => {
   const [menu, setMenu] = useState(false);
-
   return (
     <HeaderWrap>
+      <Helmet>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-82116769-4"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'UA-82116769-4');
+          `}
+        </script>
+        <script>
+          {`          
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '957464368067937');
+            fbq('track', 'PageView');
+            `}
+        </script>
+        <noscript>
+          {`          
+            <img
+              height="1"
+              width="1"
+              alt=""
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=957464368067937&ev=PageView&noscript=1"
+            />
+          `}
+        </noscript>
+      </Helmet>
+
       <div className="container">
         <Navigation role="navigation" open={menu}>
           <MobileNav>
