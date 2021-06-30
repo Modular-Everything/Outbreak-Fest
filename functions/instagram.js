f2ebeb7321d6b1d04652ea3903e576e86ce13ee9
Chrish Dunne <hey@chrish.design>
2021-06-30 11:54:55 +0100

    fix: (maybe) Fixed instagram

--- functions/instagram.js
+++ functions/instagram.js
@@ -2,8 +2,14 @@ require('isomorphic-fetch');
 
 const url = `https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables={"id":"510242604","first":8}`;
 
+const cache = {
+  lastFetch: 0,
+  posts: [],
+};
+
 function slimUpPosts(response) {
   return response.data.user.edge_owner_to_timeline_media.edges.map(edge => ({
+    biggie: edge.node.thumbnail_src,
     thumbnail: edge.node.thumbnail_resources[2].src,
     url: `https://instagram.com/p/${edge.node.shortcode}`,
     caption:
@@ -14,19 +20,17 @@ function slimUpPosts(response) {
   }));
 }
 
-const cache = {
-  lastFetch: 0,
-  posts: [],
-};
-
 async function getPosts() {
+  // first see if we have a cache in 30 mins
   const timeSinceLastFetch = Date.now() - cache.lastFetch;
-  if (timeSinceLastFetch <= 3600000) {
+  if (timeSinceLastFetch <= 1800000) {
     return cache.posts;
   }
-
   const data = await fetch(url).then(res => res.json());
   const posts = slimUpPosts(data);
+  // const posts = data;
+  cache.lastFetch = Date.now();
+  cache.posts = posts;
   return posts;
 }
 
