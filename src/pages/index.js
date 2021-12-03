94eeec06aaa3b1aa45c34fb6a20d5359dabd0d0a
Chrish Dunne <hey@chrish.design>
2021-12-03 10:07:31 +0000

    feat: Add posters to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO/SEO';
 import BigCopy from '../components/BigCopy/BigCopy';
 import Divider from '../components/Divider/Divider';
-import Video from '../components/Video/Video';
+import Poster from '../components/Poster/Poster';
 import SocialMedia from '../components/SocialMedia/SocialMedia';
 
 // ====
 
-const IndexPage = () => (
-  <Layout mask>
-    <SEO title="Home" />
+const IndexPage = () => {
+  const DATA = useStaticQuery(graphql`
+    query Posters2022Home {
+      allFile(
+        filter: { name: { regex: "/2022/" } }
+        sort: { fields: name, order: ASC }
+      ) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  `);
 
-    <Divider />
-    <BigCopy />
-    <Divider />
-    <Video />
-    <SocialMedia />
-  </Layout>
-);
+  return (
+    <Layout mask>
+      <SEO title="Home" />
+
+      <Divider />
+      <BigCopy />
+      <Divider />
+      {DATA.allFile.nodes.map(poster => (
+        <Poster key={poster.id} poster={poster.childImageSharp.fluid} />
+      ))}
+      <SocialMedia />
+    </Layout>
+  );
+};
 
 export default IndexPage;
