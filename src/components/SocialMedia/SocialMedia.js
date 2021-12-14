360a59ca74538069e18a68efac7ef8250d9955f0
Chrish Dunne <hey@chrish.design>
2021-12-14 10:21:05 +0000

    fix: Social media posts

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -11,11 +11,43 @@ import Rips from '../Rips/Rips';
 // ====
 
 const SocialMedia = () => {
+  function useInstagram() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      fetch(`/.netlify/functions/instagram`)
+        .then(res => res.json())
+        .then(data => {
+          setPosts(data);
+        });
+    }, []);
+    return posts;
+  }
+
+  const insta = useInstagram();
+
   return (
     <div style={{ zIndex: '10', position: 'relative' }}>
       <Rips>
         <div className="container">
-          <Title>Follow us on social media</Title>
+          <Title>Latest from social media</Title>
+          <Posts>
+            {!insta.length ? (
+              <Loading>Loading...</Loading>
+            ) : (
+              insta.map(gram => (
+                <li key={gram.id}>
+                  <a href={gram.url} target="_blank" rel="noreferrer noopener">
+                    <img
+                      src={`https://images.weserv.nl/?url=${encodeURIComponent(
+                        gram.thumbnail,
+                      )}&w=255`}
+                      alt={gram.caption}
+                    />
+                  </a>
+                </li>
+              ))
+            )}
+          </Posts>
           <SocialIcons>
             <li>
               <a
@@ -57,6 +89,25 @@ const Title = styled.h3`
   color: var(--primary);
 `;
 
+const Posts = styled.ul`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2rem;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+
+  & li {
+    padding: 0;
+    margin: 0;
+  }
+
+  & img {
+    width: 100%;
+  }
+`;
+
 const SocialIcons = styled.ul`
   display: flex;
   flex-direction: row;
@@ -82,4 +133,18 @@ const SocialIcons = styled.ul`
       color: var(--secondary);
     }
   }
+`;
+
+const Loading = styled.li`
+  display: block;
+  align-self: center;
+  font-family: 'EurostileExt', 'Helvetica Neue', Helvetica, sans-serif;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+  margin-top: 2rem;
+  color: var(--secondary);
+  grid-column: 2/4;
+  grid-row: 1/3;
 `;
