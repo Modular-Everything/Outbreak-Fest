7ef19cb9672d5a19afea18ba8d99ba235857a701
Chrish Dunne <hey@chrish.design>
2022-03-30 16:22:37 +0100

    fix: Page layout breaking

--- src/pages/press.js
+++ src/pages/press.js
@@ -3,20 +3,25 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO/SEO';
 import BigCopy from '../components/BigCopy/BigCopy';
+import Divider from '../components/Divider/Divider';
+import SocialMedia from '../components/SocialMedia/SocialMedia';
 
 // ====
 
 const IndexPage = () => {
   if (typeof window !== `undefined`) {
-    window.location.href = 'https://forms.gle/QtkRQrmpt6BgUgot8';
+    window.location.href = 'http://outbreakfest.twickets.live/';
   }
 
   return (
     <Layout mask>
       <SEO title="Press" />
-      <a href="https://forms.gle/QtkRQrmpt6BgUgot8">
+      <a href="http://outbreakfest.twickets.live/">
         <BigCopy custom title="Redirecting..." />
       </a>
+      <Divider />
+      <BigCopy />
+      <SocialMedia />
     </Layout>
   );
 };
