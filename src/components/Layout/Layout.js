4047f31db6a778658773c8efde5ff1f8617a5eb8
Chrish Dunne <yo@modular-everything.com>
2022-06-21 16:09:02 +0100

    feat: Remove rips

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types';
 
 import '../../styles/styles.css';
 import Texture from '../Texture/Texture';
-import Rips from '../Rips/Rips';
 
 // ====
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Rips hideTop />
       <main className="spacing__homepage">{children}</main>
       <Texture />
     </>
