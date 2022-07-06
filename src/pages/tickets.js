a67c3e8334038898192bffef21b2f3ecd23d66fa
Chrish Dunne <yo@modular-everything.com>
2022-07-06 21:47:51 +0100

    feat: Remove extra bits

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -3,8 +3,6 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO/SEO';
 import BigCopy from '../components/BigCopy/BigCopy';
-import Divider from '../components/Divider/Divider';
-import SocialMedia from '../components/SocialMedia/SocialMedia';
 
 // ====
 
@@ -19,9 +17,6 @@ const IndexPage = () => {
       <a href="https://outbreak.seetickets.com/">
         <BigCopy custom title="Redirecting..." />
       </a>
-      <Divider />
-      <BigCopy />
-      <SocialMedia />
     </Layout>
   );
 };
